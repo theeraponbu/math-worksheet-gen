a73ef13f5e2fbd7dc4cb6928fe1d8437dcf0b068
theeraponbu <theerapon.bu@gmail.com>
2026-02-17 06:23:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ st.sidebar.title("🎓 MathPrepAI Global")
 st.sidebar.markdown("---")
 menu = st.sidebar.radio(
     "SELECT TOOL:",
-    ["🏠 Home", "⚡ Speed Math Drills", "🧩 Fraction Factory", "📐 Shape Master", "📉 Graphing Notebook"]
+    ["🏠 Home", "⚡ Speed Math Drills", "🧩 Fraction Factory", "📐 Shape Master", "📉 Graphing Notebook", "🕒 Time & Clock"]
 )
 
 if menu == "🏠 Home":
@@ -39,3 +39,7 @@ elif menu == "📐 Shape Master":
 elif menu == "📉 Graphing Notebook":
     import graphing_notebook
     graphing_notebook.run_app()
+
+elif menu == "🕒 Time & Clock":
+    import clock_generator
+    clock_generator.run_app()
